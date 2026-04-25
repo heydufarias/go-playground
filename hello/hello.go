382b0ffa9316900c07629c5519984bc23144644b
dufarias <heydufarias@gmail.com>
2026-04-25 14:13:39 -0300

    feat: Enhance Hello function to support multiple languages and update tests

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchhHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
+	}
+
+	if language == french {
+		return frenchhHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Eduardo"))
+	fmt.Println(Hello("Eduardo", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,25 +4,40 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying 'Hello, Param' when adding a string", func(t *testing.T) {
-		got := Hello("Eduardo")
+		got := Hello("Eduardo", "")
 		want := "Hello, Eduardo"
 		assertCorrectMessage(t, got, want)
 
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 
 	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Eduardo", "Spanish")
+		want := "Hola, Eduardo"
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Eduardo", "French")
+		want := "Bonjour, Eduardo"
+
+		assertCorrectMessage(t, want, got)
+	})
 }
 
-func assertCorrectMessage(t testing.TB, got string, want string) {
+func assertCorrectMessage(t testing.TB, got, want string) {
 	// testing.TB is an interface from Go’s testing package
 	// It’s implemented by:
 	// *testing.T (unit tests)
 	// *testing.B (benchmarks)
 	// Meaning: this helper works for both tests and benchmarks.
+
+	// When you have more than one argument of the same type (in our case two strings) rather than having (got string, want string) you can shorten it to (got, want string)
 
 	t.Helper()
 	// t.Helper() marks the function as a helper function.
@@ -34,4 +49,5 @@ func assertCorrectMessage(t testing.TB, got string, want string) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
